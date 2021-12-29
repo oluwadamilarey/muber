e12ac64b304cdc9ed3b541dc13795fe181d8714f
Abimbola David <38018952+oluwadamilarey@users.noreply.github.com>
2021-12-30 00:58:20 +0100

    added googogle search auto-complete

--- src/screens/DestinationSearch/index.js
+++ src/screens/DestinationSearch/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import {StyleSheet, TextInput, View, SafeAreaView} from 'react-native';
 
+import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
+
 const DestinationSearch = () => {
+  const [fromText, setFromText] = React.useState('');
+  const [destinationText, setdestinationText] = React.useState('');
+
   return (
     <SafeAreaView>
       <View style={styles.container}>
-        <TextInput style={styles.textInput} placeHolder="From" />
-        <TextInput style={styles.textInput} placeHolder="Where to?" />
+        <TextInput
+          value={fromText}
+          style={styles.textInput}
+          placeholder="From"
+          onChangeText={setFromText}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Where to?"
+          onChangeText={setdestinationText}
+          value={destinationText}
+        />
+        <GooglePlacesAutocomplete
+          placeholder="Search"
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            console.log(data, details);
+          }}
+          query={{
+            key: 'AIzaSyCZRrat3B4PYHCgtpT1Nsxn200N3ezMm4k',
+            language: 'en',
+          }}
+        />
       </View>
     </SafeAreaView>
   );
@@ -19,6 +45,9 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   textInput: {
-    height: 50,
+    backgroundColor: '#eee',
+    marginVertical: 5,
+    padding: 10,
+    borderRadius: 7,
   },
 });
