39b7c318a467ff71f7ba94916f93f859da8007e2
Abimbola David <38018952+oluwadamilarey@users.noreply.github.com>
2021-12-31 08:33:00 +0100

    uber x

--- src/components/UberTypes/index.js
+++ src/components/UberTypes/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import UberTypeRow from '../UberTypeRow';
+
+// import typesData from '../../assets/data/types';
+
+const typesData = [
+  {
+    id: '0',
+    type: 'UberX',
+    price: 22,
+    duration: 45,
+  },
+  {
+    id: '1',
+    type: 'Comfort',
+    price: 27,
+    duration: 30,
+  },
+  {
+    id: '2',
+    type: 'UberXL',
+    price: 36,
+    duration: 27,
+  },
+];
 
 const UberTypes = () => {
   return (
     <View>
-      <Text>I am a UberType</Text>
+      {typesData.map(type => (
+        <UberTypeRow type={type} key={type.id} />
+      ))}
+      <UberTypeRow />
     </View>
   );
 };
