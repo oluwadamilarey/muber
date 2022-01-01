48f27bc1ea1201ee042d717fb57cba17180e9b60
Abimbola David <38018952+oluwadamilarey@users.noreply.github.com>
2022-01-01 01:58:33 +0100

    metro config

--- src/components/HomeMap/index.js
+++ src/components/HomeMap/index.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 const HomeMap = () => {
   return (
-    <View
-      style={{
-        height: 300,
-        backgroundColor: '#a0abff',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>I am a map</Text>
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}></MapView>
     </View>
   );
 };
 
 export default HomeMap;
+
+const styles = StyleSheet.create({
+  container: {
+    height: '50%',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  map: {
+    height: '100%',
+    width: '100%',
+  },
+});
